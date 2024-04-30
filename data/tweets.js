80ec6c148dbd2441a13b0d6c6588f9c5d36ff803
이광제 <zzcv00@gmail.com>
2024-04-30 10:28:55 +0900

    회원가입, 로그인 라우터 추가

--- data/tweets.js
+++ data/tweets.js
@@ -19,30 +19,42 @@ let tweets = [
 
 // 모든 트윗을 리턴
 export async function getAll() {
-
+    return tweets;
 }
 
 // 해당 아이디에 대한 트윗을 리턴
 export async function getAllByUsername(username){
-
+    return tweets.filter((tweet) => tweet.username === username);
 }
 
 // 글번호에 대한 트윗을 리턴
 export async function getByID(id){
-
+    return tweets.find((tweet) => tweet.id === id);
 }
 
 // 트윗을 작성
 export async function create(text, name, username){
-
+    const tweet = {
+        id: '10',
+        text, // 키값하고 변수값하고 같으면 한번만 적어도됨
+        createdAt: Date.now().toString(),
+        name: name, // 키값하고 변수값하고 같으면 한번만 적어도됨
+        username: username
+    };
+    tweets = [tweet, ...tweets];
+    return tweets;
 }
 
 // 트윗을 변경
 export async function update(id, text){
-
+    const tweet = tweets.find((tweet) => tweet.id === id);
+    if(tweet){
+        tweet.text = text;
+    }
+    return tweet;
 }
 
 // 트윗을 삭제
 export async function remove(id){
-
+    tweets = tweets.filter((tweet) => tweet.id !== id);
 }
