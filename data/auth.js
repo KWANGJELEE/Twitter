6008c7e6524f9594e2c83d7498d2afe7eb6c531d
이광제 <zzcv00@gmail.com>
2024-05-03 09:22:08 +0900

    토큰 검증

--- data/auth.js
+++ data/auth.js
@@ -1,29 +1,35 @@
 let users = [
     {
-        id:'1',
-        text: '안녕하세요!',
-        createdAt: Date.now().toString(),
-        name: '김사과',
+        id: '1',
         username: 'apple',
-        url: 'https://www.logoyogo.com/web/wp-content/uploads/edd/2021/02/logoyogo-1-45.jpg'
+        password: '$2b$10$ERrMBARDprS1Ajt9snjS2uXaCuYv/enG3nWFldAQWsnUQ2G.XwKvu',
+        name: '김사과',
+        email: 'apple@apple.com',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
+    },
+    {
+        id: '2',
+        username: 'banana',
+        password: '$2b$10$ERrMBARDprS1Ajt9snjS2uXaCuYv/enG3nWFldAQWsnUQ2G.XwKvu',
+        name: '반하나',
+        email: 'banana@banana.com',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
     }
 ];
-
-export async function createUser(username, password, name, email){
-    const user = {
-        id: '10',
-        username,
-        password,
-        name,
-        email,
-        url: 'https://www.logoyogo.com/web/wp-content/uploads/edd/2021/02/logoyogo-1-45.jpg'
-    }
-    users = [user, ...users]
-    return users;
+// 아이디(username) 중복검사
+export async function findByUsername(username){
+    return users.find((user) => user.username === username);
 }
-
+// id 중복검사
+export async function findById(id){
+    return users.find((user) => user.id === id);
+}
+export async function createUser(user){
+    const created = {id:'10', ...user }
+    users.push(created);
+    return created.username;
+}
 export async function login(username){
     const user = users.find((user) => user.username === username)
     return user;
 }
-
--- router/tweets.js
+++ router/tweets.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as tweetController from '../controller/tweets.js'
 import { body } from 'express-validator';
 import { validate } from "../middleware/validator.js";
+import { isAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -13,37 +14,19 @@ const validateTweet = [
     body('text').trim().isLength({min:3}).withMessage('최소 3자 이상입력'), validate
 ]
 
-let tweets = [
-    {
-        id:'1',
-        text: '안녕하세요!',
-        createdAt: Date.now().toString(),
-        name: '김사과',
-        username: 'apple',
-        url: 'https://www.logoyogo.com/web/wp-content/uploads/edd/2021/02/logoyogo-1-45.jpg'
-    }, 
-    {
-        id:'2',
-        text: '반갑습니다!',
-        createdAt: Date.now(), 
-        name: '반하나',
-        username: 'banana',
-        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
-    }
-];
 
 
 // 해당 아이디에 대한 트윗글 가져오기
 // GET
 // http://localhost:8080/tweets?username=:username
 
-router.get('/', tweetController.getTweets);
+router.get('/', isAuth, tweetController.getTweets);
 
 // 글 번호에 대한 트윗 가져오기
 // GET
 // http://localhost:8080/tweets/:id
 
-router.get('/:id', tweetController.getTweet);
+router.get('/:id', isAuth, tweetController.getTweet);
 
 // 트윗하기
 // POST
@@ -51,7 +34,7 @@ router.get('/:id', tweetController.getTweet);
 // name, username, text
 // Json형태로 입력 후 추가된 데이터까지 모두 json으로 출력
 
-router.post('/', validateTweet, tweetController.createTweet);
+router.post('/', isAuth, validateTweet, tweetController.createTweet);
 
 // 트윗 수정하기
 // PUT
@@ -59,13 +42,13 @@ router.post('/', validateTweet, tweetController.createTweet);
 // id, username, text
 // json 형태로 입력 후 변경된 데이터까지 모두 json으로 출력
 
-router.put('/:id', validateTweet, tweetController.updateTweet);
+router.put('/:id', validateTweet, isAuth, tweetController.updateTweet);
 
 // 트윗 삭제하기
 // DELETE
 // http// http://localhost:8080/tweets/:id
 
-router.delete('/:id', tweetController.deleteTweet);
+router.delete('/:id', isAuth, tweetController.deleteTweet);
 
 
 export default router;
