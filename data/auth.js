f033ad66705782d6d2d3ce9813a9d65b796e4076
이광제 <zzcv00@gmail.com>
2024-05-09 11:43:21 +0900

    Sequelize 적용

--- data/auth.js
+++ data/auth.js
@@ -1,32 +1,54 @@
-import { db } from '../db/database.js';
+import SQ from 'sequelize';
+import { sequelize } from '../db/database.js';
+const DataTypes = SQ.DataTypes;   // SQ 에서 사용하는 데이터형을 넣어줌
+
+export const User = sequelize.define(
+    'user',   // 테이블 생성때 자동으로 s 붙음
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING(150),
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        url: DataTypes.STRING(1000)        
+    },
+    { timestamps: false }
+);
+
 
 // 아이디(username) 중복검사
 export async function findByUsername(username){
-    return db.execute('select * from users where username = ?', [username]).then
-    ((result) => {
-        console.log(result);
-        return result[0][0];  
-});
+    return User.findOne({where: {username}});
 }
 
 // id 중복검사
 export async function findById(id){
-    return db.execute('select * from users where id = ?', [id]).then((result) => { console.log(result);
-        return result[0][0];
-    });
+    return User.findByPk(id);
 }
-
 
 export async function createUser(user){
-    const {username, hashed, name, email, url} = user;
-    return db.execute('insert into users (username, password, name, email, url) values (?, ?, ?, ?, ?)', [username, hashed, name, email, url]).then ((result) => {
-        console.log(result);    // result[0].insertedId
-        return result[0];
-    });
+    return User.create(user).then((data) => data.dataValues.id)
 }
 
 
-export async function login(username){
-    const user = users.find((user) => user.username === username)
-    return user;
-}
+// export async function login(username){
+//     const user = users.find((user) => user.username === username)
+//     return user;
+// }
